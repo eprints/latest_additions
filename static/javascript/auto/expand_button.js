f7e66cb1f2d10d769edeb217ec4d31e1fcb7b26d
fatchild <edoakleymail@gmail.com>
2023-12-19 14:37:31 +0000

    ejo - rearranged for integration with bootstrap

--- static/javascript/auto/expand_button.js
+++ static/javascript/auto/expand_button.js
@@ -1,11 +1,14 @@
 function expandNames(button) {
   var fullNames = button.parentNode.nextElementSibling;
-  var expandIcon =  button.querySelector('img');
-    if (fullNames.style.display === 'none') {
-      fullNames.style.display = 'block';
-      expandIcon.src = '/javascript/images/minus.png';
+  var expandIcon = button.querySelector('#expand-icon'); 
+  var closeIcon = button.querySelector('#close-icon'); 
+    if (expandIcon.style.display !== 'none') {
+	fullNames.style.display = 'block';
+	expandIcon.style.display = 'none';
+	closeIcon.style.display = 'unset';
     } else {
-      fullNames.style.display = 'none';
-      expandIcon.src = '/javascript/images/plus.png';
+	fullNames.style.display = 'none';
+	expandIcon.style.display = 'unset';
+	closeIcon.style.display = 'none';
     }
 }
