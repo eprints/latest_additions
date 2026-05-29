01f1a66f2d5feeba13d703d291bbff65bcfe4e86
David R Newman <drn@ecs.soton.ac.uk>
2026-05-29 13:52:35 +0000

    Updates README and copyright/licensing boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Latest Additions
-This ingredient was originally intended to provide a widget so you could display the latest deposited items as a grid of document previews with citations, typically on a repository homepage.  However, it now serves a more generic purpose of displaying these preview/citation grids for whatever set you can define.
+
+This ingredient was originally intended to provide a widget so you could display the latest deposited items as a 
+grid of document previews with citations, typically on a repository homepage.  However, it now serves a more 
+generic purpose of displaying these preview/citation grids for whatever set you can define.
+
+## Author(s)
+- Ed Oakley, EPrints Services
+
+## Copyright and Licensing
+The files contained within this 'latest_additions' ingredient are Copyright (2023-2026) of the University of Southampton and provided under the GNU Lesser General Public License (https://www.gnu.org/licenses/lgpl-3.0.en.html)
+
+The TinyMCE files contained within the static directory are supplied and redistributed under the GNU Lesser GPL License v2.1.
+
--- cfg.d/z_latest_tool.pl
+++ cfg.d/z_latest_tool.pl
@@ -32,32 +32,11 @@ $c->{latest_tool_feeds} = {
 
 $c->{latest_tool_tile_zoom} = 1;
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-=for COPYRIGHT BEGIN
+=begin COPYRIGHT_AND_LICENSE
 
-Copyright 2022 University of Southampton.
-EPrints 3.4 is supplied by EPrints Services.
+Copyright University of Southampton under the GNU Lesser General Public License. See README on https://github.com/eprints/latest_additions for further information.
+Latest Additions ingredient is supplied by EPrints Services.
 
-http://www.eprints.org/eprints-3.4/
-
-=for COPYRIGHT END
-
-=for LICENSE BEGIN
-
-This file is part of EPrints 3.4 L<http://www.eprints.org/>.
-
-EPrints 3.4 and this file are released under the terms of the
-GNU Lesser General Public License version 3 as published by
-the Free Software Foundation unless otherwise stated.
-
-EPrints 3.4 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with EPrints 3.4.
-If not, see L<http://www.gnu.org/licenses/>.
-
-=for LICENSE END
+=end COPYRIGHT_AND_LICENSE
